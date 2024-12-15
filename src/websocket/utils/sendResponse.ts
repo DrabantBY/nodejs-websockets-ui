60b3d588194bdd19438d71097bdb0d1957848a8c
DrabantBY <drabant2022@gmail.com>
2024-12-15 19:08:57 +0300

    refactor: add sendResponse util

---
+++ src/websocket/utils/sendResponse.ts
@@ -0,0 +1,29 @@
+import websockets from '../db/websockets.ts';
+import stringifyData from './stringifyData.ts';
+import pointers from '../db/pointers.ts';
+import users from '../db/users.ts';
+import type { QueryTypes } from '../types/common.ts';
+
+const sendResponse = (
+	type: QueryTypes,
+	data: any,
+	ids?: (string | number)[]
+): void => {
+	const response = stringifyData({
+		id: 0,
+		type,
+		data,
+	});
+
+	if (!ids || !ids.length) {
+		Object.values(users).forEach(({ index }) => {
+			websockets[pointers[index]]?.send(response);
+		});
+	} else {
+		ids.forEach((id) => {
+			websockets[pointers[id]]?.send(response);
+		});
+	}
+};
+
+export default sendResponse;
