c41f43f1bf9ff0f1b5a0f1869f4a30fd759c6022
DrabantBY <drabant2022@gmail.com>
2024-12-16 05:48:28 +0300

    feat: add next guard method

--- src/websocket/services/GuardService.ts
+++ src/websocket/services/GuardService.ts
@@ -5,36 +5,44 @@ import type {
 	CreateRoomRequest,
 	AddShipsRequest,
 	AttackRequest,
+	SinglePlayRequest,
 } from '../types/requests.ts';
+import { TYPE } from '../types/requests.ts';
 
 export default class GuardService {
 	static isRegRequest(
 		request: Common<unknown, unknown>
 	): request is LoginRequest {
-		return request.type === 'reg';
+		return request.type === TYPE.REG_USER;
 	}
 
 	static isCreateRoomRequest(
 		request: Common<unknown, unknown>
 	): request is CreateRoomRequest {
-		return request.type === 'create_room';
+		return request.type === TYPE.ADD_ROOM;
 	}
 
 	static isAddToRoomRequest(
 		request: Common<unknown, unknown>
 	): request is AddToRoomRequest {
-		return request.type === 'add_user_to_room';
+		return request.type === TYPE.ADD_USER;
 	}
 
 	static isAddShipsRequest(
 		request: Common<unknown, unknown>
 	): request is AddShipsRequest {
-		return request.type === 'add_ships';
+		return request.type === TYPE.ADD_SHIP;
 	}
 
 	static isAttackRequest(
 		request: Common<unknown, unknown>
 	): request is AttackRequest {
-		return request.type === 'attack' || request.type === 'randomAttack';
+		return request.type === TYPE.ATTACK || request.type === TYPE.RANDOM;
+	}
+
+	static isSinglePlayRequest(
+		request: Common<unknown, unknown>
+	): request is SinglePlayRequest {
+		return request.type === TYPE.SINGLE;
 	}
 }
--- src/websocket/types/requests.ts
+++ src/websocket/types/requests.ts
@@ -6,19 +6,21 @@ export type Common<T, D> = {
 	data: D;
 };
 
-enum TYPE {
+export enum TYPE {
 	REG_USER = 'reg',
 	ADD_ROOM = 'create_room',
 	ADD_SHIP = 'add_ships',
 	ADD_USER = 'add_user_to_room',
 	ATTACK = 'attack',
 	RANDOM = 'randomAttack',
+	SINGLE = 'single_play',
 }
 
 type ID = { indexRoom: number | string };
 
 export type LoginRequest = Common<TYPE.REG_USER, Login>;
 export type CreateRoomRequest = Common<TYPE.ADD_ROOM, ''>;
+export type SinglePlayRequest = Common<TYPE.SINGLE, ''>;
 export type AddShipsRequest = Common<TYPE.ADD_SHIP, Player>;
 export type AttackRequest = Common<TYPE.ATTACK | TYPE.RANDOM, Attack>;
 export type AddToRoomRequest = Common<TYPE.ADD_USER, ID>;
